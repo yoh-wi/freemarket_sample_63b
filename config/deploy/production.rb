0b81a101d0b49dc50867e251780fb647f1164be3
ryuji-ito <eposodeskip@yahoo.co.jp>
2020-02-15 20:05:27 +0900

    deploy commit #12

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,8 +6,10 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+server '3.20.45.137', user: 'ec2-user', roles: %w{app db web}
 
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
@@ -59,5 +61,3 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-
-server '3.20.45.137', user: 'ec2-user', roles: %w{app db web}
