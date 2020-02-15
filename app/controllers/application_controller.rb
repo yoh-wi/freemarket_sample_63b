b43375f3ba05879c80de368bd29661e0dfa699c4
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-02-15 12:05:47 +0900

    謎のコードをコメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  #before_action :basic_auth
   protect_from_forgery with: :exception
 
   private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
+  #以下のコードいつの間にかあったけどよくわからないのでコメントアウトしちゃいます（by大庭）
+  #def basic_auth
+    #authenticate_or_request_with_http_basic do |username, password|
+      #username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    #end
+  #end
 end
