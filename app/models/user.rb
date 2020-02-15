d79ef04941fb5da05c3628c588d6dd3fef879f61
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-02-15 14:58:33 +0900

    全角のみのバリデーションをかける

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :last_name_kanji, :first_name_kanji, :last_name_kana, :first_name_kana, :birthday, :tel, presence: true
+  validates :last_name_kanji, :first_name_kanji, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
 end
