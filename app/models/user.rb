529b5188dca73cb867efccbca056ab366cadac78
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-02-15 12:30:31 +0900

    devise日本語化！

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  varidates :nickname, :last_name_kanji, :first_name_kanji, :last_name_kana, :first_name_kana, :birthday, :tel, presence: true
+  validates :nickname, :last_name_kanji, :first_name_kanji, :last_name_kana, :first_name_kana, :birthday, :tel, presence: true
 end
