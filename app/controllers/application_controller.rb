070f1046b12434942779b31af1c0fefc712ae78c
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-02-15 12:17:33 +0900

    追加したカラムも登録できるようapplication_controllerへ記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   #before_action :basic_auth
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name_kanji, :first_name_kanji, :last_name_kana, :first_name_kana, :birthday, :tel, :profile])
+  end
 
   private
 
