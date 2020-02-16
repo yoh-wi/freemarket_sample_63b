7cf02d9f3212f55129eeaf28306a53842fb9ed5b
ryuji-ito <eposodeskip@yahoo.co.jp>
2020-02-16 21:44:29 +0900

    deploy commit #13 S3 if

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,22 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
-    aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
-    region: 'ap-northeast-1'
-  }
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
+      aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
+      region: 'ap-northeast-1'
+    }
 
-  config.fog_directory  = 'freemarketsample63b'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample63b'
+    config.fog_directory  = 'freemarketsample63b'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample63b'
+
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
+
 end
