764ec130bd5bfeb3e3dccd008a01eb95007725d3
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-02-19 15:28:10 +0900

    住所登録のためのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'edit_profile', to: 'users/registrations#edit_profile', as: 'edit_profile'
     patch 'update_profile', to: 'users/registrations#update_profile', as: 'update_profile'
+    get 'new_address', to: 'users/registrations#new_address', as: 'new_address'
+    post 'create_address', to: 'users/registrations#create_address', as: 'create_address'
+    get 'edit_address', to: 'users/registrations#edit_address', as: 'edit_address'
+    patch 'update_profile', to: 'users/registration#update_address', as: 'update_address'
   end
 
   root to: "home#index"
