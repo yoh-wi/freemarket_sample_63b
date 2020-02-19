1a762f51f44498bb2e43708f5d986d955edcf1da
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-02-19 15:31:54 +0900

    addressがあるか？の条件分岐のボタンを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_one :address
+  has_one :address, dependent: :destroy
+  accepts_nested_attributes_for :address
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :last_name_kanji, :first_name_kanji, :last_name_kana, :first_name_kana, :birthday, :tel, presence: true
   validates :last_name_kanji, :first_name_kanji, format: {with: /\A[ぁ-んァ-ン一-龥]/}
   validates :last_name_kana, :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+
 end
