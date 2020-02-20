9cb4b17fcca9eaf22079ceab6ea1cbed5a505865
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-02-20 20:33:12 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
+  namespace :users do
+    resources :address, only: [:new, :create, :edit, :update]
+  end
   devise_scope :user do
     get 'edit_profile', to: 'users/registrations#edit_profile', as: 'edit_profile'
     patch 'update_profile', to: 'users/registrations#update_profile', as: 'update_profile'
-    get 'new_address', to: 'users/registrations#new_address', as: 'new_address'
-    post 'create_address', to: 'users/registrations#create_address', as: 'create_address'
-    get 'edit_address', to: 'users/registrations#edit_address', as: 'edit_address'
-    patch 'update_profile', to: 'users/registration#update_address', as: 'update_address'
   end
 
   root to: "home#index"
