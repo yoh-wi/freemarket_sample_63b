9edee8d4e3a3aa9d71636092cc4c3b167fe81a4a
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-02-21 18:14:05 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-  #以下のコードいつの間にかあったけどよくわからないのでコメントアウトしちゃいます（by大庭）
   #def basic_auth
     #authenticate_or_request_with_http_basic do |username, password|
       #username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
