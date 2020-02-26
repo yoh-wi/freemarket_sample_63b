35e5870343be7ef0df8eb830cbb718577f95dfe6
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-02-26 18:20:09 +0900

    brand_flgカラムは不要だと思ったので削除

--- db/migrate/20200226083518_create_categories.rb
+++ db/migrate/20200226083518_create_categories.rb
@@ -2,7 +2,6 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string    :name,      null: false
-      t.boolean   :brand_flg, default:false, null: false
       t.timestamps
     end
   end
