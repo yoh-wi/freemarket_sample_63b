b4c03c03325394eb5d9759b21927e77923ff6b4d
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-03-01 16:21:30 +0900

    各モデルのアソシエーションを定義

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  belongs_to :category
+  belongs_to :shipping_payer_method
+  belongs_to :seller, class_name: 'User'
+  belongs_to :buyer, class_name: 'User'
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
