b242533097038d322a4aee8f388b15516ffb48a1
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-03-02 18:36:50 +0900

    コントローラでnew、createアクションを定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,5 +4,20 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+  end
+  
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :category_id, :brand, :product_condition, :shippng_payer_method_id, :prefecture_id, :days_of_shipping, :price, :trade_status).merge(seller_id: current_user.id)
   end
 end
