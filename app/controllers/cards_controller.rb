987917d5289e2e58ff8fd4b0dbde6a9b53c86916
yoh-wi <57764555+yoh-wi@users.noreply.github.com>
2020-03-02 21:03:22 +0900

    markup-card-registration

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,9 @@
 class CardsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
     get 'edit_profile', to: 'users/registrations#edit_profile', as: 'edit_profile'
     patch 'update_profile', to: 'users/registrations#update_profile', as: 'update_profile'
   end
-  resources :users
+  resources :users, only: [:show, :edit, :destroy]
+  resources :cards, only: [:index, :new, :create]
   # root to:"users#login"
-  root to: 'users#edit'
+  root to: 'cards#new'
   
 end
