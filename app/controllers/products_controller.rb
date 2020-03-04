110aa5b4a9bb0b7c79edbaf96a55698bfb23cf70
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-03-04 18:54:46 +0900

    product.newと同時にimageもnewできるようにコントローラーを編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @product.images.build
     @parent_category = Category.where(ancestry: nil)
     @payer = ShippingPayerMethod.where(ancestry: nil)
   end
@@ -32,6 +33,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :description, :category_id, :brand, :product_condition, :shippng_payer_method_id, :prefecture_id, :days_of_shipping, :price, :trade_status).merge(seller_id: current_user.id)
+    params.require(:product).permit(:name, :description, :category_id, :brand, :product_condition, :shippng_payer_method_id, :prefecture_id, :days_of_shipping, :price, :trade_status, images_attributes:[:image]).merge(seller_id: current_user.id)
   end
 end
