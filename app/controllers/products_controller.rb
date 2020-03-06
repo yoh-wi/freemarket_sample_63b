574bc99178e52fc9646b802b042a93e4ba7961e1
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-03-06 11:56:37 +0900

    paramsの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,8 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    @parent_category = Category.where(ancestry: nil)
+    @payer = ShippingPayerMethod.where(ancestry: nil)
     if @product.save
       redirect_to root_path
     else
@@ -33,6 +35,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :description, :category_id, :brand, :product_condition, :shippng_payer_method_id, :prefecture_id, :days_of_shipping, :price, :trade_status, images_attributes:[:image]).merge(seller_id: current_user.id)
+    params.require(:product).permit(:id, :name, :description, :category_id, :brand, :product_condition, :shipping_payer_method_id, :prefecture_id, :days_of_shipping, :price, :trade_status, images_attributes:[:image, :_destroy, :id]).merge(seller_id: current_user.id)
   end
 end
