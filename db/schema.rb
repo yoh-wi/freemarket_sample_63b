e4d5f18de149ae20632cc56c4103a7a320567c40
ryuji-ito <eposodeskip@yahoo.co.jp>
2020-03-08 17:04:53 +0900

    マイグレート後

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_03_01_080243) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
-    t.integer "product_id"
+    t.integer "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
