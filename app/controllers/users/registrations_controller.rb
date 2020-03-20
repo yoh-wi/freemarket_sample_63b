b785f350a3b71d640e3f2adbeb77cc61a741aef8
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-03-20 16:35:29 +0900

    誕生日の部分を修正
    sprintfを使うとparamsで渡されるデータが"birthday(1i)"=>"1985", "birthday(2i)"=>"3", "birthday(3i)"=>"22",のようになってbirthdayカラムに保存できない。コントローラーでdate型に直すアクションを追加しました。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :birthday_join, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -37,6 +37,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :profile])
   end
 
+  def birthday_join
+    # パラメータ取得
+    date = params[:user]
+    binding.pry
+    # ブランク時のエラー回避のため、ブランクだったら何もしない
+    if date["birthday(1i)"].empty? && date["birthday(2i)"].empty? && date["birthday(3i)"].empty?
+      return
+    end
+    # 年月日別々できたものを結合して新しいDate型変数を作って返す
+    params[:user][:birthday] = Date.new date["birthday(1i)"].to_i,date["birthday(2i)"].to_i,date["birthday(3i)"].to_i
+  end
   # PUT /resource
   # def update
   #   super
