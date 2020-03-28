2893fe829cd8d87c38fa853aaf9de58ddd8f9fdf
ryuji-ito <eposodeskip@yahoo.co.jp>
2020-03-28 14:39:32 +0900

    rails-controller-testing install

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe CardsController, type: :controller do
 
   describe 'GET #index' do
+    before do
+      FactoryBot.build(:user)
+      FactoryBot.build(:card)
+    end
+  
     it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe ProductsController, type: :controller do
 
   describe 'GET #buy_complete' do
+    before do
+      FactoryBot.build(:card)
+      FactoryBot.build(:product)
+    end
     it 'returns http success' do
       get :create
       expect(response).to have_http_status(:success)
