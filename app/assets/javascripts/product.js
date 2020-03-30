d06689f8b9a9c994d959a998cb52a80b5407f5bb
yoh-wi <57764555+yoh-wi@users.noreply.github.com>
2020-03-30 18:46:47 +0900

    コメントアウト削除と修正

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -283,7 +283,4 @@ $(document).on('turbolinks:load', ()=> {
   $('.detail-small__list--image').on('mouseover', function(){
     $('.detail-box__body__content__list--image').attr({src:$(this).attr('src'),alt:$(this).attr('alt')});
   });
-  // $(document).ready(function() {
-  //   $('#previews').append(buildFileField(fileIndex[0]));
-  // });
 });
