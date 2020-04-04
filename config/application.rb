bf00296f71dc67c26c78a61f394dc25e46f649b5
ryuji-ito <eposodeskip@yahoo.co.jp>
2020-04-04 16:57:09 +0900

    field_with_errorsの再々修正

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module FreemarketSample63b
 
     config.i18n.default_locale = :ja
 
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
