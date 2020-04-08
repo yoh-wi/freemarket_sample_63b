8aa2ef3f364c0d5a3d0e49a7ceb5703cb6a9e723
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-04-08 14:51:03 +0900

    ビュー調整

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,7 +21,6 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    binding.pry
     if @product.save
       redirect_to root_path
     else
