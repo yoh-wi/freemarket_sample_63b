ef745d29694bcb79c1af394a82bd99786199a749
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-04-18 02:36:50 +0900

    挙動確認&調整

--- app/assets/javascripts/product_validation.js
+++ app/assets/javascripts/product_validation.js
@@ -1,5 +1,6 @@
 $(function(){
-  $('.form__content__btn--submit').on('click', function() {
+  $('.form__content__btn--submit').on('click', function(e) {
+    e.preventDefault();
     //挿入するエラーメッセージ
     var inputErrorHtml = `<div class='error_message'>入力してください</div>`;
     var selectErrorHtml = `<div class='error_message'>選択してください</div>`;
@@ -7,7 +8,7 @@ $(function(){
     var imageErrorHtml = `<div class='error_message'>画像がありません</div>`;
     var priceErrorHtml = `<div class='attention'>300以上9999999以下で入力してください</div>`;
     //各入力内容を取得
-    var image = $('.js-preview').val();
+    var image = $('.js-preview').size();
     var name = $('.form__content__name--field').val();
     var description = $('.form__content__description--area').val();
     var grandchildCategory = $('#grandchild_category').val();
@@ -20,12 +21,12 @@ $(function(){
     //バリデーション判定用変数
     let judge = 0
     //バリデーションチェック
-    if (image = 'undefined') {
+    if (image === 0) {
       $('.form__content__image > .error_message').remove();
       $('.form__content__image').append(imageErrorHtml);
       judge += 1
     }
-    if (grandchildCategory = 'undefined' || grandchildCategory == 0) {
+    if (grandchildCategory === undefined || grandchildCategory == 0) {
       $('.form__content__category > .error_message').remove();
       $('.form__content__category').append(selectErrorHtml);
       judge += 1
@@ -44,7 +45,7 @@ $(function(){
       $('.form__content__description').append(descriptionErrorHtml);
       judge += 1
     }
-    if (size == 0) {
+    if (size == '') {
       $('#size_wrapper > .error_message').remove();
       $('#size_wrapper').append(selectErrorHtml);
       judge =+ 1
@@ -54,7 +55,11 @@ $(function(){
       $('.form__content__condition').append(selectErrorHtml);
       judge += 1
     }
-    if (method = 'undefined' || method == 0) {
+    if (method === undefined) {
+      $('.form__content__method > .error_message').remove();
+      $('.form__content__method').append(selectErrorHtml);
+      judge += 1
+    } else if (method == '') {
       $('.form__content__method > .error_message').remove();
       $('.form__content__method').append(selectErrorHtml);
       judge += 1
@@ -79,7 +84,6 @@ $(function(){
       $('#sell_form').submit();
     } else {
       var position = $('.error_message').parent().get(0).offsetTop
-      console.log(position);
       $('html,body').animate({ scrollTop: position });
     }
   })
@@ -92,16 +96,25 @@ $(function(){
       $('.form__content__name > .error_message').remove();
     }
   })
-  $('#size').on('change', function(){
+  $('.form__content__category').on('change', '#size',function(){
     $('#size_wrapper > .error_message').remove();
   })
-  $('#parent_category, #child_category, #grandchild_category').on('change', function(){
+  $('#parent_category').on('change', function(){
+    $('.form__content__category > .error_message').remove();
+  })
+  $('.form__content__category').on('change', '#child_category', function(){
+    $('.form__content__category > .error_message').remove();
+  })
+  $('.form__content__category').on('change', '#grandchild_category', function(){
     $('.form__content__category > .error_message').remove();
   })
   $('.form__content__condition--field').on('change', function(){
     $('.form__content__condition > .error_message').remove();
   })
-  $('#payer, #method').on('change', function(){
+  $('#payer').on('change', function(){
+    $('.form__content__method > .error_message').remove();
+  })
+  $('.form__content__method').on('change', '#method', function(){
     $('.form__content__method > .error_message').remove();
   })
   $('.form__content__area--field').on('change', function(){
